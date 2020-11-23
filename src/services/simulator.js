a84017d1b6c7efd716d80649dd7a7d920e8b6e2f
Eric Toshio <toshio@pop-os.localdomain>
2020-11-22 21:31:01 -0300

    Add noiseSource for simulator

--- src/services/simulator.js
+++ src/services/simulator.js
@@ -2,6 +2,7 @@ const { CronJob } = require('cron');
 const { findSensorPositions } = require('../db/metric/noiseRepository');
 
 // Just for refence
+// eslint-disable-next-line no-unused-vars
 const messageTemplate = {
   // number: identifier of sensor
   id: null,
@@ -39,18 +40,45 @@ const messageCronFrequence = '*/2 * * * *';
 
 const generateRandomDecibel = () => 70 + 50 * Math.random();
 
+const calculateDecibel = (soundSource, sensor) => {
+  const dx = (soundSource.lat - sensor.lat) ** 2;
+  const dy = (soundSource.long - sensor.long) ** 2;
+  return soundSource.value / (dx + dy);
+};
+
+const generateSoundSource = (sensorPositions) => {
+  const lat = (sensorPositions.reduce((prev, actual) => prev + actual.lat, 0) / 3)
+    + 0.001 * (Math.random() * 2 - 1);
+  const long = sensorPositions.reduce((prev, actual) => prev + actual.long, 0) / 3
+    + 0.001 * (Math.random() * 2 - 1);
+  const intensity = generateRandomDecibel();
+
+  return {
+    lat,
+    long,
+    value: intensity,
+  };
+};
+
 const startSimulator = async (dbClient) => {
   const messageCollection = dbClient.db('metric').collection('message');
+  const noiseSourceCollection = dbClient.db('metric').collection('noiseSource');
   console.log('Started sensor simulator');
   const sensorPositions = await findSensorPositions(dbClient);
 
-  new CronJob(messageCronFrequence, () => {
+  new CronJob(messageCronFrequence, async () => {
     const currentTimestamp = new Date().getTime();
+    const soundSource = generateSoundSource(sensorPositions);
+    const { insertedId } = await noiseSourceCollection.insertOne({
+      ...soundSource,
+      timestamp: currentTimestamp,
+    });
     const messages = sensorPositions.map(({ _id: sensorId, ...rest }) => ({
       sensorId,
       ...rest,
+      noiseSource: insertedId,
       timestamp: currentTimestamp,
-      value: generateRandomDecibel(),
+      value: calculateDecibel(soundSource, rest),
     }));
     messageCollection.insertMany(messages);
   }, null, true, 'America/Los_Angeles').start();
